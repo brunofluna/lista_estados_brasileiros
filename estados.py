fa4d4c73c6629c661f956facb4408759e843892a
Aluno Manhã <bruno@brunoluna.com.br>
2024-08-02 17:45:12 -0300

    Versão Brasileira Bruno Luna 1.0

---
+++ estados.py
@@ -0,0 +1,6 @@
+# lista de estados 
+estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espirito Santo", "Goiás", "Maranhão", "Mato Grosso do Sul", "Mato Grosso", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"]
+
+print(f'\nQuantidade de Estados do Brasil: {len(estados)}.')
+for estado in estados:
+    print(f'\n{estado}')
